1e65e8fdaf3cd7ed92f8c0c02dcbf7f3394014d8
mahmudh <mahmudhssn246@gmail.com>
2016-02-02 13:14:15 +0000

    test passed. controller and view for stocks created

--- spec/features/shopping_cart_feature_spec.rb
+++ spec/features/shopping_cart_feature_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 feature 'shopping cart' do
-  context 'no clothing item has been added' do
-    scenario 'should display a prompt to add an item' do
-      visit '/clothing'
+  context 'no stock item has been added' do
+    scenario 'should display a prompt to add to stock' do
+      visit '/stocks'
       expect(page).to have_content 'No clothing items availiable'
-      expect(page).to have_link 'Add an item'
+      expect(page).to have_link 'Add to stock'
     end
   end
 end
