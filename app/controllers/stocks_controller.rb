e4cea641969f0460b9c3783f534d259ab9fa63d1
mahmudh <mahmudhssn246@gmail.com>
2016-02-02 16:29:13 +0000

    can add new items to stock

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,4 +2,18 @@ class StocksController < ApplicationController
   def index
     @stocks = Stock.all
   end
+
+  def new
+  end
+
+  def create
+    Stock.create(stock_params)
+    redirect_to '/stocks'
+  end
+
+  private
+
+  def stock_params
+    params.require(:stock).permit(:name, :category, :price, :qty)
+  end
 end
--- spec/features/shopping_cart_feature_spec.rb
+++ spec/features/shopping_cart_feature_spec.rb
@@ -5,7 +5,7 @@ feature 'shopping cart' do
     scenario 'should display a prompt to add to stock' do
       visit '/stocks'
       expect(page).to have_content 'No clothing items availiable'
-      expect(page).to have_link 'Add to stock'
+      expect(page).to have_link 'Add new item to stock'
     end
   end
 
@@ -19,6 +19,19 @@ feature 'shopping cart' do
       expect(page).to have_content 'Almond Toe Court Shoes, Patent Black'
       expect(page).not_to have_content 'No clothing items availiable'
     end
+  end
 
+  context 'adding items to stock' do
+    scenario 'prompts user to fill out a form, then displays the new item' do
+      visit '/stocks'
+      click_link 'Add new item to stock'
+      fill_in 'Name', with: 'Almond Toe Court Shoes, Patent Black'
+      fill_in 'Category', with: 'Women\'s footwear'
+      fill_in 'Price', with: 99.00
+      fill_in 'Qty', with: 5
+      click_button 'Create item'
+      expect(page).to have_content 'Almond Toe Court Shoes'
+      expect(current_path).to eq '/stocks'
+    end
   end
 end
